77d3304a49996a436fe6c5e358267e9bd0285c3a
yumekiti <yumekiti120440@gmail.com>
2022-05-08 02:05:54 +0900

    `prettier`

--- laravel/app/Http/Controllers/ScrapingController.php
+++ laravel/app/Http/Controllers/ScrapingController.php
@@ -10,73 +10,80 @@ class ScrapingController extends Controller
   public function signin(Request $request)
   {
     // データの初期化
-    $data = '';   // リンク
+    $data = ""; // リンク
 
     // Goutte\Client
     $client = new Client();
 
     try {
       // ログイン処理
-      $login_page = $client->request('GET', (string)env('APP_LOGIN'));
-      $login_form = $login_page->filter('form')->form();
-      $login_form['id'] = $request->input('id');
-      $login_form['pw'] = $request->input('pw');
+      $login_page = $client->request("GET", (string) env("APP_LOGIN"));
+      $login_form = $login_page->filter("form")->form();
+      $login_form["id"] = $request->input("id");
+      $login_form["pw"] = $request->input("pw");
       $client->submit($login_form);
 
-      $crawler = $client->request('GET', (string)env('APP_LOGIN'));
+      $crawler = $client->request("GET", (string) env("APP_LOGIN"));
 
       // データの取得
-      $data = $crawler->filter('.home_back')->text();
+      $data = $crawler->filter(".home_back")->text();
 
-      if($data){
-        return response(['status' => 200, 'message' => 'success'], 200);
+      if ($data) {
+        return response(["status" => 200, "message" => "success"], 200);
       }
     } catch (\Exception $e) {
-      return response(['status' => 401, 'message' => 'unauthorized error'], 401);
+      return response(
+        ["status" => 401, "message" => "unauthorized error"],
+        401
+      );
     }
   }
 
   public function news(Request $request)
   {
     // データの初期化
-    $links = array();   // リンク
-    $titles = array();  // タイトル
-    $dates = array();   // 日付
-    $tags = array();  // タグ
-    $ids = array();   // id
+    $links = []; // リンク
+    $titles = []; // タイトル
+    $dates = []; // 日付
+    $tags = []; // タグ
+    $ids = []; // id
 
     // Goutte\Client
     $client = new Client();
 
     // ログイン処理
-    $login_page = $client->request('GET', (string)env('APP_LOGIN'));
-    $login_form = $login_page->filter('form')->form();
-    $login_form['id'] = $request->input('id');
-    $login_form['pw'] = $request->input('pw');
+    $login_page = $client->request("GET", (string) env("APP_LOGIN"));
+    $login_form = $login_page->filter("form")->form();
+    $login_form["id"] = $request->input("id");
+    $login_form["pw"] = $request->input("pw");
     $client->submit($login_form);
 
     // サイトデータ取得
-    $crawler = $client->request('GET', (string)env('APP_NEWS'));
+    $crawler = $client->request("GET", (string) env("APP_NEWS"));
 
     // データの取得
-    $crawler->filter('ul.news_list01 li')->each(function ($node) use (&$links, &$titles, &$tags, &$dates, &$ids) {
-      $titles[] = $node->filter('dd')->text(); // タイトル
-      $dates[] = strstr($node->filter('dt')->text(), ' ', true); // date 空白から左
-      $tags[] = $node->filter('span')->text(); // タグ
-      $links[] = (string)env('APP_NEWS_LINK') . strstr($node->filter('a')->attr('href'), '?'); // リンク
-      $ids[] = preg_replace('/[^0-9]/', '', $node->filter('a')->attr('href')); // id urlから数字のみ
-    });
+    $crawler
+      ->filter("ul.news_list01 li")
+      ->each(function ($node) use (&$links, &$titles, &$tags, &$dates, &$ids) {
+        $titles[] = $node->filter("dd")->text(); // タイトル
+        $dates[] = strstr($node->filter("dt")->text(), " ", true); // date 空白から左
+        $tags[] = $node->filter("span")->text(); // タグ
+        $links[] =
+          (string) env("APP_NEWS_LINK") .
+          strstr($node->filter("a")->attr("href"), "?"); // リンク
+        $ids[] = preg_replace("/[^0-9]/", "", $node->filter("a")->attr("href")); // id urlから数字のみ
+      });
 
     // 取得したデータをjsonにする
-    foreach($ids as $i => $id){
+    foreach ($ids as $i => $id) {
       $params[] = [
-        'id' => $id,      // id
-        'title' => $titles[$i], // タイトル
-        'date' => $dates[$i],   // 日付
-        'tag' => $tags[$i],   // タグ
-        'link' => $links[$i],   // リンク
+        "id" => $id, // id
+        "title" => $titles[$i], // タイトル
+        "date" => $dates[$i], // 日付
+        "tag" => $tags[$i], // タグ
+        "link" => $links[$i], // リンク
       ];
-    };
+    }
     $datas = json_encode($params, JSON_UNESCAPED_UNICODE);
 
     // jsonにしたdatasを返す
@@ -86,46 +93,51 @@ class ScrapingController extends Controller
   public function only(Request $request, $id)
   {
     // データの初期化
-    $title = '';  // タイトル
-    $data = '';   // 内容
-    $date = '';   // 日にち
-    $tag = '';    // タグ
-    $attachments = array();  // リンク
+    $title = ""; // タイトル
+    $data = ""; // 内容
+    $date = ""; // 日にち
+    $tag = ""; // タグ
+    $attachments = []; // リンク
 
     // Goutte\Client
     $client = new Client();
 
     // ログイン処理
-    $login_page = $client->request('GET', (string)env('APP_LOGIN'));
-    $login_form = $login_page->filter('form')->form();
-    $login_form['id'] = $request->input('id');
-    $login_form['pw'] = $request->input('pw');
+    $login_page = $client->request("GET", (string) env("APP_LOGIN"));
+    $login_form = $login_page->filter("form")->form();
+    $login_form["id"] = $request->input("id");
+    $login_form["pw"] = $request->input("pw");
     $client->submit($login_form);
 
     // サイトデータ取得
-    $crawler = $client->request('GET', (string)env('APP_NEWS_FRONT') . $id . (string)env('APP_NEWS_REAR'));
+    $crawler = $client->request(
+      "GET",
+      (string) env("APP_NEWS_FRONT") . $id . (string) env("APP_NEWS_REAR")
+    );
 
     // データの取得
-    $title = $crawler->filter('.title')->text(); // タイトル
-    $data = $crawler->filter('.news div ~ div')->text(); // 内容
-    $date = strstr($crawler->filter('.detail_title01')->text(), ' ', true); // 日にち
-    $tag = $crawler->filter('.icon01')->text(); // タグ
-    $attachments = explode('<a', $crawler->filter('.news')->html());
+    $title = $crawler->filter(".title")->text(); // タイトル
+    $data = $crawler->filter(".news div ~ div")->text(); // 内容
+    $date = strstr($crawler->filter(".detail_title01")->text(), " ", true); // 日にち
+    $tag = $crawler->filter(".icon01")->text(); // タグ
+    $attachments = explode("<a", $crawler->filter(".news")->html());
 
     // 解体
     array_shift($attachments);
     array_pop($attachments);
-    foreach($attachments as $i => $attachment){
-      $attachments[$i] = (string)env('APP_DOMAIN') . substr(mb_strstr(substr($attachment, 7), 'class', true), 0, -2);
+    foreach ($attachments as $i => $attachment) {
+      $attachments[$i] =
+        (string) env("APP_DOMAIN") .
+        substr(mb_strstr(substr($attachment, 7), "class", true), 0, -2);
     }
 
     // 取得したデータをjsonにする
     $params = [
-      'title' => $title,  // タイトル
-      'data' => $data,  // 内容
-      'date' => $date,  // 日にち
-      'tag' => $tag,    // タグ
-      'attachments' => $attachments,
+      "title" => $title, // タイトル
+      "data" => $data, // 内容
+      "date" => $date, // 日にち
+      "tag" => $tag, // タグ
+      "attachments" => $attachments,
     ];
     $datas = json_encode($params, JSON_UNESCAPED_UNICODE);
 
@@ -136,49 +148,63 @@ class ScrapingController extends Controller
   public function calendar($year, $month, Request $request)
   {
     // データの初期化
-    $days = array();  // 日にち
-    $plans = array();  // タイトル
+    $days = []; // 日にち
+    $plans = []; // タイトル
 
     // Goutte\Client
     $client = new Client();
 
     // ログイン処理
-    $login_page = $client->request('GET', (string)env('APP_LOGIN'));
-    $login_form = $login_page->filter('form')->form();
-    $login_form['id'] = $request->input('id');
-    $login_form['pw'] = $request->input('pw');
+    $login_page = $client->request("GET", (string) env("APP_LOGIN"));
+    $login_form = $login_page->filter("form")->form();
+    $login_form["id"] = $request->input("id");
+    $login_form["pw"] = $request->input("pw");
     $client->submit($login_form);
 
     // サイトデータ取得
-    $crawler = $client->request('GET', (string)env('APP_CALENDAR') . '/index.php?c=schedule&cal_yy=' . (string)$year . '&cal_mm=' . (string)$month);
+    $crawler = $client->request(
+      "GET",
+      (string) env("APP_CALENDAR") .
+        "/index.php?c=schedule&cal_yy=" .
+        (string) $year .
+        "&cal_mm=" .
+        (string) $month
+    );
 
     // データの取得
-    $crawler->filter('ul.calendar_list01 li')->each(function ($node) use (&$days, &$plans) {
-      $days[] = $node->filter('.day')->text(); // 日にち
-      // str_replace('amp;', '', (string)env('APP_CALENDAR') . 'url')
-      $titles = explode(' ', $node->filter('p + p')->text());
-      $links = explode('</a>', explode('<p>', $node->html())[1]);
-      array_pop($links);
+    $crawler
+      ->filter("ul.calendar_list01 li")
+      ->each(function ($node) use (&$days, &$plans) {
+        $days[] = $node->filter(".day")->text(); // 日にち
+        // str_replace('amp;', '', (string)env('APP_CALENDAR') . 'url')
+        $titles = explode(" ", $node->filter("p + p")->text());
+        $links = explode("</a>", explode("<p>", $node->html())[1]);
+        array_pop($links);
 
-      foreach($links as $i => $link){
-        $links[$i] = str_replace('amp;', '', (string)env('APP_CALENDAR') . substr(mb_strstr(substr($link, 12), 'style', true), 0, -2));
-      }
+        foreach ($links as $i => $link) {
+          $links[$i] = str_replace(
+            "amp;",
+            "",
+            (string) env("APP_CALENDAR") .
+              substr(mb_strstr(substr($link, 12), "style", true), 0, -2)
+          );
+        }
 
-      $plans[] = [
-        'title' => $titles, // タイトル
-        'link' => $links,  // リンク
-      ];
-    });
+        $plans[] = [
+          "title" => $titles, // タイトル
+          "link" => $links, // リンク
+        ];
+      });
 
     // 取得したデータをjsonにする
-    foreach($days as $i => $day){
-      if($plans[$i]['title'][0]){
+    foreach ($days as $i => $day) {
+      if ($plans[$i]["title"][0]) {
         $params[] = [
-          'day' => $day, // 日にち
-          'plans' => $plans[$i],  // タイトル
+          "day" => $day, // 日にち
+          "plans" => $plans[$i], // タイトル
         ];
       }
-    };
+    }
     $datas = json_encode($params, JSON_UNESCAPED_UNICODE);
 
     // jsonにしたdatasを返す
@@ -188,35 +214,49 @@ class ScrapingController extends Controller
   public function attendance(Request $request)
   {
     // データの初期化
-    $titles = array();  // タイトル
-    $rates = array();   // 率
+    $titles = []; // タイトル
+    $rates = []; // 率
 
     // Goutte\Client
     $client = new Client();
 
     // ログイン処理
-    $login_form = $client->request('GET', (string)env('FALCON_LOGIN'));
-    $token = strstr(strstr($client->getHistory()->current()->getUri(), '('), ')', true).'))';
-    $login_form = $login_form->selectButton('ログイン')->form();
-    $login_form['txtUserId'] = $request->input('id');
-    $login_form['txtPassword'] = $request->input('pw');
+    $login_form = $client->request("GET", (string) env("FALCON_LOGIN"));
+    $token =
+      strstr(
+        strstr(
+          $client
+            ->getHistory()
+            ->current()
+            ->getUri(),
+          "("
+        ),
+        ")",
+        true
+      ) . "))";
+    $login_form = $login_form->selectButton("ログイン")->form();
+    $login_form["txtUserId"] = $request->input("id");
+    $login_form["txtPassword"] = $request->input("pw");
     $client->submit($login_form);
 
     // サイトデータ取得
-    $crawler = $client->request('GET', (string)env('FALCON_FRONT') . $token . (string)env('FALCON_ATTENDANCE'));
+    $crawler = $client->request(
+      "GET",
+      (string) env("FALCON_FRONT") . $token . (string) env("FALCON_ATTENDANCE")
+    );
 
     // データの取得
-    $crawler->filter('a')->each(function ($node) use (&$titles, &$rates) {
-      $titles[] = strstr($node->text(), ' ', true); // タイトル
-      $rates[] = trim(strstr($node->text(), ' ')); // 率
+    $crawler->filter("a")->each(function ($node) use (&$titles, &$rates) {
+      $titles[] = strstr($node->text(), " ", true); // タイトル
+      $rates[] = trim(strstr($node->text(), " ")); // 率
     });
 
     // 取得したデータをjsonにする
-    foreach($titles as $i => $title){
-      if($title){
+    foreach ($titles as $i => $title) {
+      if ($title) {
         $params[] = [
-          'title' => $title,    // タイトル
-          'rate' => $rates[$i],   // 率
+          "title" => $title, // タイトル
+          "rate" => $rates[$i], // 率
         ];
       }
     }
@@ -229,39 +269,77 @@ class ScrapingController extends Controller
   public function timetable(Request $request)
   {
     // データの初期化
-    $dates = array();  // 日にち
-    $titles = array();  // 日にち
+    $dates = []; // 日にち
+    $titles = []; // 日にち
 
     // Goutte\Client
     $client = new Client();
 
     // ログイン処理
-    $login_form = $client->request('GET', (string)env('FALCON_LOGIN'));
-    $token = strstr(strstr($client->getHistory()->current()->getUri(), '('), ')', true).'))';
-    $login_form = $login_form->selectButton('ログイン')->form();
-    $login_form['txtUserId'] = $request->input('id');
-    $login_form['txtPassword'] = $request->input('pw');
+    $login_form = $client->request("GET", (string) env("FALCON_LOGIN"));
+    $token =
+      strstr(
+        strstr(
+          $client
+            ->getHistory()
+            ->current()
+            ->getUri(),
+          "("
+        ),
+        ")",
+        true
+      ) . "))";
+    $login_form = $login_form->selectButton("ログイン")->form();
+    $login_form["txtUserId"] = $request->input("id");
+    $login_form["txtPassword"] = $request->input("pw");
     $client->submit($login_form);
 
     // サイトデータ取得
-    $crawler = $client->request('GET', (string)env('FALCON_FRONT') . $token . (string)env('FALCON_TIMETABLE'));
+    $crawler = $client->request(
+      "GET",
+      (string) env("FALCON_FRONT") . $token . (string) env("FALCON_TIMETABLE")
+    );
 
     // データの取得
-    $crawler->filter('div')->each(function ($node) use (&$dates) {
-      if(strpos($node->text(),'/')){
+    $crawler->filter("div")->each(function ($node) use (&$dates) {
+      if (strpos($node->text(), "/")) {
         $dates[] = $node->text(); // 日にち
       }
     });
 
     // 取得したデータをjsonにする
-    foreach($dates as $i => $date){
+    foreach ($dates as $i => $date) {
       $params[] = [
-        'date' => strstr($date, '(', true), // 日にち
-        'weekday' => ltrim(trim(strstr(strstr($date, '('), ')', true)), '('),
-        'timetable' => []
+        "date" => strstr($date, "(", true), // 日にち
+        "weekday" => ltrim(trim(strstr(strstr($date, "("), ")", true)), "("),
+        "timetable" => [],
       ];
-      $datas = array_filter(str_replace('限:', '', preg_split('/[0-9]/', preg_replace("/( |　)/", "", str_replace('行事 ', '', strstr(strstr($client->click($crawler->selectLink($date)->link())->text(), '行事'), '戻る', true))))));
-      $params[$i]['timetable'] += $datas;
+      $datas = array_filter(
+        str_replace(
+          "限:",
+          "",
+          preg_split(
+            "/[0-9]/",
+            preg_replace(
+              "/( |　)/",
+              "",
+              str_replace(
+                "行事 ",
+                "",
+                strstr(
+                  strstr(
+                    $client->click($crawler->selectLink($date)->link())->text(),
+                    "行事"
+                  ),
+                  "戻る",
+                  true
+                )
+              )
+            )
+          )
+        )
+      );
+      $params[$i]["timetable"] += $datas;
     }
     $datas = json_encode($params, JSON_UNESCAPED_UNICODE);
 
